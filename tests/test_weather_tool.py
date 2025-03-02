412dfe377480d7880040bc64f1712bb2c6522f03
TamerOnLine <tamer@tameronline.com>
2025-03-02 05:20:20 +0100

    Key

--- tests/test_weather_tool.py
+++ tests/test_weather_tool.py
@@ -6,6 +6,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../s
 import pytest
 from src.tools.weather_tool import get_weather
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+assert API_KEY is not None, "API_KEY is missing. Please check your environment variables."
+
+
 def test_weather_tool():
     response = get_weather("Berlin")
     assert isinstance(response, str)
